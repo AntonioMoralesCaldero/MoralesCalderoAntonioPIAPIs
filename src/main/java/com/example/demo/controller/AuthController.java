58d88d016619734f435fda247f5bdb274c750c00
AntonioMoralesCaldero <antoniommoralescaldero@gmail.com>
2024-12-09 22:41:29 +0100

    login problemas

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -5,44 +5,32 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.model.UsuarioModel;
 import com.example.demo.security.JwtUtil;
-import com.example.demo.service.CustomUserDetailsService;
 import com.example.demo.service.UsuarioService;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 
-	private final CustomUserDetailsService customUserDetailsService;
-    private final JwtUtil jwtUtil;
     private final UsuarioService usuarioService;
+    private final JwtUtil jwtUtil;
 
     @Autowired
-    public AuthController(CustomUserDetailsService customUserDetailsService, JwtUtil jwtUtil, UsuarioService usuarioService) {
-        this.customUserDetailsService = customUserDetailsService;
-        this.jwtUtil = jwtUtil;
+    public AuthController(UsuarioService usuarioService, JwtUtil jwtUtil) {
         this.usuarioService = usuarioService;
+        this.jwtUtil = jwtUtil;
     }
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestParam String username, @RequestParam String password) {
-        try {
-            // Intenta cargar al usuario o administrador
-            UserDetails userDetails = customUserDetailsService.loadUserByUsername(username);
-
-            // Verifica la contraseña manualmente (puedes usar bcrypt si está configurado)
-            if (userDetails.getPassword().equals(password)) {
-                // Genera un token JWT para cualquier usuario válido
-                String token = jwtUtil.generateToken(userDetails.getUsername());
-                return ResponseEntity.ok(Map.of("message", "Inicio de sesión exitoso", "token", token));
-            } else {
-                return ResponseEntity.status(401).body("Usuario o contraseña incorrectos.");
-            }
-        } catch (Exception e) {
+        UsuarioModel usuario = usuarioService.login(username, password);
+        if (usuario != null) {
+            String token = jwtUtil.generateToken(usuario.getUsername());
+            return ResponseEntity.ok(Map.of("message", "Inicio de sesión exitoso", "token", token));
+        } else {
             return ResponseEntity.status(401).body("Usuario o contraseña incorrectos.");
         }
     }
